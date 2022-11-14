2c7806bc77bfaa7d6236efa70f46a874baf99654
Khaled Salhab <khaled.salhab@forescout.com>
2022-11-14 16:28:29 +0200

    first commit

---
+++ src/main/java/com/ksalhab/caragency/CaragencyApplication.java
@@ -0,0 +1,13 @@
+package com.ksalhab.caragency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaragencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaragencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksalhab/caragency/CaragencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksalhab.caragency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaragencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
